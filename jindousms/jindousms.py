e7fac1a8ef50148783506311e18aedbf10db1c95
luckywheat <44030497+luckywheat@users.noreply.github.com>
2020-10-04 22:32:38 +0800

    Update jindousms.py

--- jindousms/jindousms.py
+++ jindousms/jindousms.py
@@ -102,7 +102,7 @@ def get_user_information():
 
 if __name__ == "__main__":
     base_url = 'http://www.jindousms.com'
-    apikey = 'cbeda47e5041edc146b3f3d0bfce1e54'
+    apikey = ''
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.68'
     }
